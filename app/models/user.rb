3a905c85448cbe49314ef4b30ac9eb17a0d89f30
naozinn <85996931+naozinn@users.noreply.github.com>
2021-09-23 19:50:12 +0900

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,8 +11,8 @@ class User < ApplicationRecord
 
   validates :introduction,length: {  maximum: 50}
   
-  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy # フォロー取得
-  has_many :relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy # フォロワー取得
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy # フォロー取得
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy # フォロワー取得
   
   has_many :followers, through: :reverse_of_relationships, source: :follower # 自分がフォローしている人
   has_many :followings, through: :relationships, source: :followed # 自分をフォローしている人
